cb77f4e68722ac1430369b828fea68e1e0e05e05
Abigiya Terefe <abigiya1122@gmail.com>
2023-07-17 08:18:08 +0300

    deleted un needed parts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import React from 'react';
+import Header from './components/header';
+import AddTodo from './components/AddTodo';
+import { BrowserRouter as Router,Routes,Navigate, Route,Outlet, Switch, Link } from 'react-router-dom';
+import Fof from './components/Login';
+import SignUp from './components/Signup';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+const App = () => {
+	const aute=true
+	return (
+		<Router>
+			<div className='container'>
+				<div className='home'>
+						<Header aute={aute} />
+					<Routes>
+					<Route path='/login' element={<Fof />} />
+					<Route path='/sugnup' element={<SignUp />} />
+						<Route
+							element={
+								aute ? <Outlet /> : <Navigate to={'/login'} replace />
+							}
+						>
+							<Route path={'/'} element={<AddTodo />} />
+						</Route>
+					</Routes>
+				</div>
+			</div>
+			</Router>
+	);
+
+};
 
 export default App;
