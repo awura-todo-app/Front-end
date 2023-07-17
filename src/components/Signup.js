b82e3c6b5a641628b16ac5eb02e34a080a9ae3a3
Abigiya Terefe <abigiya1122@gmail.com>
2023-07-17 08:29:41 +0300

    sign up page complited

---
+++ src/components/Signup.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./FOF.css"
+const SignUp = ()=>{
+  const [value, setValue] = useState()
+  const [password, setPassword] = useState()
+  return(
+    <div className='FOF-container'>
+    <div className='FOF-group3'>
+    <input
+					type="text"
+					placeholder="uset name or email"
+					value={value}
+					className='FOF-input FOF-input-email'
+					onChange={(event) => setValue(event.target.value)}
+				></input>
+    <input
+					type="text"
+					placeholder="password"
+					value={password}
+					className='FOF-input FOF-input-password'
+					onChange={(event) => setPassword(event.target.value)}
+				></input>
+				<button className="FOF-button" >
+			 		<span className='addTodo-button-text'>Sign up</span>
+			 	</button>
+    </div>
+  </div>
+  )
+}
+
+export default SignUp;
